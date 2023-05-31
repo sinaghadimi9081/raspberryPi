dee6c461a564dc862d42c1c250bfce4c023934b1
[sinaghadimi9081] <[s.ghadimi@outlook.com]>
2023-05-30 22:20:52 -0700

    bcm

--- LED/blue.py
+++ LED/blue.py
@@ -1,8 +1,8 @@
 import RPi.GPIO as GPIO
 from time import sleep
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(40,GPIO.OUT)
-mypwm = GPIO.PWM(40,100)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(21,GPIO.OUT)
+mypwm = GPIO.PWM(21,100)
 try:
     while True:
         mypwm.start(50)
