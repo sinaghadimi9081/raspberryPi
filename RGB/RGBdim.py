55011bfe3c632d116884e29bc7ee83461f3f0b0f
sinaghadimi9081 <s.ghadimi@outlook.com>
2023-05-30 23:35:00 -0700

    4

--- RGB/RGBdim.py
+++ RGB/RGBdim.py
@@ -4,7 +4,7 @@ GPIO.setmode(GPIO.BCM)
 
 rPin=21
 rPin_out=26
-rcounter=0.99
+rcounter=0.4
 rOld=1
 GPIO.setup(rPin,GPIO.IN,pull_up_down=GPIO.PUD_UP)
 GPIO.setup(rPin_out,GPIO.OUT)
@@ -12,7 +12,7 @@ rPWM=GPIO.PWM(rPin_out,100)
 
 gPin=20
 gPin_out=19
-gcounter=0.99
+gcounter=0.4
 gOld=1
 GPIO.setup(gPin,GPIO.IN,pull_up_down=GPIO.PUD_UP)
 GPIO.setup(gPin_out,GPIO.OUT)
@@ -20,7 +20,7 @@ gPWM=GPIO.PWM(gPin_out,100)
 
 bPin=16
 bPin_out=13
-bcounter=0.99
+bcounter=0.4
 bOld=1
 GPIO.setup(bPin,GPIO.IN,pull_up_down=GPIO.PUD_UP) 
 GPIO.setup(bPin_out,GPIO.OUT)
@@ -32,8 +32,8 @@ try:
         greenRead=GPIO.input(gPin)
         blueRead=GPIO.input(bPin)
         rPWM.start(rcounter)
-        rPWM.start(gcounter)
-        rPWM.start(bcounter)
+        gPWM.start(gcounter)
+        bPWM.start(bcounter)
         if redRead==1 and rOld==0:
             print("I am reading red")
             rcounter *=1.58
