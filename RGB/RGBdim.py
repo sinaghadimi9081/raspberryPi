e56a47711c02188d17a3c416d8d8556a2b5bf39d
[sinaghadimi9081] <[s.ghadimi@outlook.com]>
2023-05-31 01:55:29 -0700

    2

--- RGB/RGBdim.py
+++ RGB/RGBdim.py
@@ -4,7 +4,7 @@ GPIO.setmode(GPIO.BCM)
 
 rPin=21
 rPin_out=26
-rcounter=0.4
+rcounter=0.99
 rOld=1
 GPIO.setup(rPin,GPIO.IN,pull_up_down=GPIO.PUD_UP)
 GPIO.setup(rPin_out,GPIO.OUT)
@@ -12,7 +12,7 @@ rPWM=GPIO.PWM(rPin_out,100)
 
 gPin=20
 gPin_out=19
-gcounter=0.4
+gcounter=0.99
 gOld=1
 GPIO.setup(gPin,GPIO.IN,pull_up_down=GPIO.PUD_UP)
 GPIO.setup(gPin_out,GPIO.OUT)
@@ -20,7 +20,7 @@ gPWM=GPIO.PWM(gPin_out,100)
 
 bPin=16
 bPin_out=13
-bcounter=0.4
+bcounter=0.99
 bOld=1
 GPIO.setup(bPin,GPIO.IN,pull_up_down=GPIO.PUD_UP) 
 GPIO.setup(bPin_out,GPIO.OUT)
@@ -33,20 +33,20 @@ try:
         blueRead=GPIO.input(bPin)
         if redRead==1 and rOld==0:
             print("I am reading red")
-            rcounter *=1.58
+            rcounter =rcounter*1.58
             if rcounter>95:
                 rcounter=0.99
             rPWM.ChangeDutyCycle(int(rcounter))
         if greenRead==1 and gOld==0:
             print("I am reading green")
-            gcounter *=1.58
+            gcounter =gcounter*1.58
             if gcounter>95:
                 gcounter=0.99
             gPWM.ChangeDutyCycle(int(gcounter))
         if blueRead==1 and bOld==0:
             print("I am reading blue")
             rPWM.start(bcounter)
-            bcounter *=1.58
+            bcounter =bcounter*1.58
             if bcounter>95:
                 bcounter=0.99
             bPWM.ChangeDutyCycle(int(bcounter))
