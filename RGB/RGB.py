8723bb88d9b2f2a6da332f576e99e6e32b90bdb2
sinaghadimi9081 <s.ghadimi@outlook.com>
2023-06-01 01:30:47 -0700

    ee

---
+++ RGB/RGB.py
@@ -0,0 +1,9 @@
+import RPi.GPIO as GPIO
+from time import sleep
+try:
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(38,GPIO.OUT)
+    GPIO.output(38,1)
+except KeyboardInterrupt:
+    GPIO.cleanup()
+
