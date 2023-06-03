40912b6757ae9fc274d577c4724dc250ab89ece2
[sinaghadimi9081] <[s.ghadimi@outlook.com]>
2023-06-03 00:30:10 -0700

    analog

---
+++ analogsignal.py
@@ -0,0 +1,13 @@
+import RPi.GPIO as GPIO
+from time import sleep
+import ADC0834
+
+GPIO.setmode(GPIO.BCM)
+ADC0834.setup()
+try:
+    while True:
+        analogVal=ADC0834.getResult(0)
+        print(analogVal)
+        sleep(0.2)
+except KeyboardInterrupt:
+    GPIO.cleanup()
