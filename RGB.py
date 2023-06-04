af3029647dfc646953b8a48aa24e783e0b990494
[sinaghadimi9081] <[s.ghadimi@outlook.com]>
2023-06-03 23:18:41 -0700

    1

--- RGB.py
+++ RGB.py
@@ -1,9 +1,10 @@
 import RPi.GPIO as GPIO
 from time import sleep
 try:
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(26,GPIO.OUT)
-    GPIO.output(26,1)
+    while True:
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(26,GPIO.OUT)
+        GPIO.output(26,1)
 except KeyboardInterrupt:
     GPIO.cleanup()
 
